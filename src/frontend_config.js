f11782a41b2646510b4593ed1d3c3b5a19e60c83
WadeStern <wademstern@gmail.com>
2023-07-31 19:43:14 -0400

    updated dns

--- src/frontend_config.js
+++ src/frontend_config.js
@@ -1,5 +1,6 @@
 var configs = {
-    backendurl : 'buypotato-backend',
+    //backendurl : 'buypotato-backend',
+    backendurl : 'backend.prod.mstern.com',
     backendport : '7200'
 };
 module.exports = {configs};
